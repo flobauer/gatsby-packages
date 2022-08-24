656e8aae9a37baeff460be391b77d756c9dad0af
Florian Bauer <florian@sbg.at>
2022-08-24 15:22:59 +0200

    Add solution fro hanserikb

--- packages/gatsby-plugin-use-query-params/root.js
+++ packages/gatsby-plugin-use-query-params/root.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Location, globalHistory } from "@reach/router";
+import { navigate } from "gatsby";
 import { QueryParamProvider } from "use-query-params";
+
+export const GatsbyAdapter = ({ children }) => {
+  const [adapter] = useState(() => ({
+    get location() {
+      return globalHistory.location;
+    },
+    push(location) {
+      navigate(location.search || "?", { replace: false });
+    },
+    replace(location) {
+      navigate(location.search || "?", { replace: true });
+    },
+  }));
+
+  return children(adapter);
+};
 
 export default ({ children }) => (
   <Location>
     {({ location }) => (
-      <QueryParamProvider location={location} reachHistory={globalHistory}>
+      <QueryParamProvider location={location} reachHistory={globalHistory} adapter={GatsbyAdapter}>
         {children}
       </QueryParamProvider>
     )}
   </Location>
 );
+x;
